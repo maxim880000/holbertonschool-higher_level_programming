db0a8a4d0b219f36f5174e8ef0b5390a88c323f6
maxim880000 <mdholberton@gmail.com>
2026-01-14 15:45:52 +0100

    committed 1. The last digit

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import random
+# random number display his last digit
+# Génère un nombre aléatoire
+number = random.randint(-10000, 10000)
+
+# % 10 donne le dernier chiffre
+last_digit = number % 10
+
+# On corrige pour avoir le vrai dernier chiffre avec le signe
+if number < 0 and last_digit != 0:
+    last_digit = -last_digit   # -626 → on veut -6 et pas 4
+
+# Affichage de la phrase de base
+print(f"Last digit of {number} is {last_digit}", end=" ")
+
+# différentes conditions demandées
+if last_digit > 5:
+    print("and is greater than 5")
+
+elif last_digit == 0:
+    print("and is 0")
+
+else:
+    # Ici on a soit :
+    #    1,2,3,4,5 ou -1,-2,-3,-4,-5
+    print("and is less than 6 and not 0")
