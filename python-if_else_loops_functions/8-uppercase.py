424573f5be3a98ce475e129a796b591ca58271b1
maxim880000 <mdholberton@gmail.com>
2026-01-15 13:44:41 +0100

    committted correctio of python code style on task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,15 +3,13 @@
 # Fonction qui affiche une chaîne en majuscules suivie d'un saut de ligne
 def uppercase(str):
 
-    # On parcourt chaque caractère de la chaîne
+    # Parcours chaque caractère de la chaîne
     for c in str:
         # Si le caractère est une lettre minuscule ('a' à 'z')
         if ord(c) >= 97 and ord(c) <= 122:
-            # On affiche le caractère converti en majuscule
-            # chr(ord(c) - 32) transforme 'a'-'z' en 'A'-'Z'
-            print("{}".format(chr(ord(c) - 32)), end="")
-        else:
-            # Si ce n'est pas une minuscule, on l'affiche tel quel
-            print("{}".format(c), end="")
-    # Après avoir affiché toute la chaîne, on saute une ligne
+            # Convertit la minuscule en majuscule en utilisant ASCII
+            c = chr(ord(c) - 32)
+        # Affiche le caractère (majuscules ou non) sans passer à la ligne
+        print("{}".format(c), end="")
+    # Après toute la chaîne, affiche un saut de ligne
     print()
