0ab578b39b5ee4eb5d21ac2969d00b2f56e4a586
maxim880000 <mdholberton@gmail.com>
2026-01-15 15:25:49 +0100

    committed prints the last digit of a number.

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# prints the last digit of a number.
+def print_last_digit(number):
+
+	# abs pour les nb negatifs
+	last_digit = abs(number) % 10
+	# afficher le dernier chiffre sans saut de ligne
+	print("{}".format(last_digit), end="")
+	   
+	return last_digit
