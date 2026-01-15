cb8832757dddd12b5077478a02a9ccdb2a9344b1
maxim880000 <mdholberton@gmail.com>
2026-01-15 10:41:27 +0100

    committed correction of 2-printalph pycodestyle

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Boucle sur les codes ASCII de 97 à 122 (a,z)
 for i in range(97, 123):
-	# chr(i) convertit le code ASCII en caractère
+    # chr(i) convertit le code ASCII en caractère
     # print end="" affiche le caractère sans aller à la ligne
-	print("{}".format(chr(i)), end="")
+    print("{}".format(chr(i)), end="")
