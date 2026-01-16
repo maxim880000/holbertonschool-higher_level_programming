c66cfc8024ffee251245426395fe45ad3d6e5d2f
maxim880000 <mdholberton@gmail.com>
2026-01-16 10:54:06 +0100

    commited  3. Infinite addition

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv  # Pour récupérer les arguments passés au script
+
+if __name__ == "__main__":
+    total = 0  # On initialise le total à zéro
+
+    # On parcourt tous les arguments sauf le nom du fichier
+    for arg in argv[1:]:
+        total += int(arg)
+        # On convertit chaque argument en entier et on l'ajoute au total
+
+    print(total)  # On affiche le résultat final
