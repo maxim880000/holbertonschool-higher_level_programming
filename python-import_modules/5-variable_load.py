7decaf26cfe9fe3ef7385270f34a1656385d6729
maxim880000 <mdholberton@gmail.com>
2026-01-16 10:58:43 +0100

    committed  5. Everything can be imported

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from variable_load_5 import a  # On importe uniquement la variable 'a'
+
+if __name__ == "__main__":
+    # S’assure que ce code ne s’exécute que si lancé directement
+    print(a)  # Affiche la valeur de 'a'
