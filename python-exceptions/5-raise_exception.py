4827088fe68fa8a6d445205f35d2aeaaf9e486cc
maxim880000 <mdholberton@gmail.com>
2026-01-22 10:43:42 +0100

    committe duntil task 6

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Function that raises a TypeError
+def raise_exception():
+    raise TypeError  # déclenche directement une exception de type TypeError
---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Function that raises a NameError with a custom message
+def raise_exception_msg(message=""):
+    # déclenche une exception NameError avec le message fourni
+	raise NameError(message)
