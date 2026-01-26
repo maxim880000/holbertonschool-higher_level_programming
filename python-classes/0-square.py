0eccb98ae646b7882628ffe12abe4fd281a26683
maxim880000 <mdholberton@gmail.com>
2026-01-26 13:19:10 +0100

    committed 0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Définition d'une classe appelée Square
+# Pour l'instant la classe est vide (elle ne fait rien)
+# Le mot-clé pass sert juste à dire à Python : "il n'y a rien ici"
+
+class Square:
+    pass
