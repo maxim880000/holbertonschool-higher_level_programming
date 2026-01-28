dea24939d3821ef1d2cf33a4fd2d4761147dd891
maxim880000 <mdholberton@gmail.com>
2026-01-28 09:44:17 +0100

    until task 2

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# objet qu'on creer = self
+# valeur usr tape pour creation du carré = size
+class Square:
+    def __init__(self, size):
+        # __init__ constructeur (creer un objet ini ses attributs)
+        self.__size = size
+        # creer un attribut privé (__) __size
