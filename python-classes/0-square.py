b0d3aa6be7e9287b835b9ed3930fc3603cb7f42b
maxim880000 <mdholberton@gmail.com>
2026-01-28 09:53:12 +0100

    committed correction of 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""Définition d'une classe appelée Square (vide)
+pass sert juste à dire à Python : "il n'y a rien ici"""
+
 
 class Square:
     pass
-
-# Définition d'une classe appelée Square (vide)
-# pass sert juste à dire à Python : "il n'y a rien ici"
