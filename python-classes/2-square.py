bc985456eac9c7a81e9c49f8ac1111309c7760aa
maxim880000 <mdholberton@gmail.com>
2026-01-28 14:31:16 +0100

    .

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
+"""Module qui définit une classe Square représentant un carré"""
+
 
 class Square:
-	def __init__(self, size=0):
-		
+    """Classe qui permet de créer et manipuler un carré"""
+
+    def __init__(self, size=0):
+        """Initialise un nouvel objet Square"""
+        """size représente la longueur d’un côté du carré
+        Il doit obligatoirement être un entier positif ou nul"""
+
+        # Vérifie que size est bien un entier
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        # Vérifie que size n'est pas négatif
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        # Attribut privé qui stocke la taille du carré
+        self.__size = size
