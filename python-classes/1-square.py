7078ff1bba6bd3bc659b9d48b31ca714a59451b0
maxim880000 <mdholberton@gmail.com>
2026-01-28 10:10:54 +0100

    committed correction task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-# objet qu'on creer = self
-# valeur usr tape pour creation du carré = size
+"""objet qu'on creer = self"""
+"""valeur usr tape pour creation du carré = size"""
+
+
 class Square:
     def __init__(self, size):
-        # __init__ constructeur (creer un objet ini ses attributs)
+        """__init__ constructeur (creer un objet ini ses attributs)"""
         self.__size = size
-        # creer un attribut privé (__) __size
+        """creer un attribut privé (__) __size"""
