ae42d1d2df733cf5728c8ecff332beed38f45a0b
maxim880000 <mdholberton@gmail.com>
2026-02-04 10:04:06 +0100

    .

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
+
 def lookup(obj):
     """return the list of available attributes and methods of an object
-thank to the fonction dir"""
+    thank to the fonction dir"""
     return (dir(obj))
