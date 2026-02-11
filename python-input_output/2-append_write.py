b3ee1e36a143aa192348e96a0c2fc0e23799b240
maxim880000 <mdholberton@gmail.com>
2026-02-11 10:33:39 +0100

    correction task 3  about pycodestyle

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """appends a string at the end of a text file"""
 
+
 def append_write(filename="", text=""):
     """appends a string at the end of a text file"""
     with open(filename, 'a', encoding="utf-8") as file:
