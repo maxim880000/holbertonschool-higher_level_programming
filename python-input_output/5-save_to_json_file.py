52620acfa43c1add63c47730f1b245667451dbdd
maxim880000 <mdholberton@gmail.com>
2026-02-11 10:34:42 +0100

    correction task 5  about pycodestyle

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,7 +6,7 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a JSON representation"""
-    with open(filename, 'w', encoding = "utf-8") as file:
+    with open(filename, 'w', encoding="utf-8") as file:
         # json.dump(quoi, où)
         # transforme objpy en json et ecrit dans un fichier
         json.dump(my_obj, file)
