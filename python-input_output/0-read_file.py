34d7332f4f208dfb8ef8e652fbc3b8455c574e5b
maxim880000 <mdholberton@gmail.com>
2026-02-11 10:22:55 +0100

    adding .format on task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,5 @@
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as file:
-        print(file.read())
+        # file.read lire tout le fichier
+        print("{}".format(file.read()))
