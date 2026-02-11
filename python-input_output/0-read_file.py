1307f3ef8506c71850880101557f668c810d3be3
maxim880000 <mdholberton@gmail.com>
2026-02-11 09:57:42 +0100

    .

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,7 @@
 
 
 def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as file:
         # file.read lire tout le fichier
         print(file.read())
