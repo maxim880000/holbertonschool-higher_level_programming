a4f54345676072d9414b0129c4f75bc08006a940
maxim880000 <mdholberton@gmail.com>
2026-02-11 10:20:10 +0100

    correction of task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-# ecrit un string dans un fichier txt (return nb char)
-# UTF-8 sert a transformer les lettres en 0 et 1
+"""writes a string to a text file (UTF8) and return nb of char"""
 # 'w' ecrire dans un fichier
 
 
 def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) and return nb of char"""
     with open(filename, 'w', encoding="utf-8") as file:
-        file.write(text)
-    return len(text)
+        return file.write(text)
