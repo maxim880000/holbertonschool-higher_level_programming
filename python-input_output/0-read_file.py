8d4fc5713e80494f65f5d6da7f14ca305287e4c5
maxim880000 <mdholberton@gmail.com>
2026-02-12 20:51:58 +0100

    correction of task 0 of IO

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""Module that reads a UTF-8 text file and prints it to stdout"""
-# file: vraible qui represente le fichier ouvert
+"""Module qui définit une fonction pour lire un fichier texte
+et afficher son contenu sur la sortie standard.
+"""
 
 
 def read_file(filename=""):
-    """reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, "r", encoding="utf-8") as file:
-        # file.read lire tout le fichier
-        print(file.read())
+    """Lit un fichier UTF-8 et affiche son contenu.
+
+    Utilise l'instruction 'with' pour s'assurer que le fichier
+    est fermé correctement après la lecture.
+        filename (str): chemin du fichier à lire. Par défaut, vide.
+    """
+    # Ouverture du fichier en mode lecture ('r') avec encodage UTF-8
+    with open(filename, 'r', encoding='utf-8') as f:
+        # Lire tout le contenu du fichier et l'afficher
+        print(f.read(), end="")  # end="" pour ne pas ajouter de nouvelle ligne supplémentaire
