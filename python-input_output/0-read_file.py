4f74de1bca634edddb136ed2891d533bca16d6a1
maxim880000 <mdholberton@gmail.com>
2026-02-12 20:54:29 +0100

    correction of task 0 of IO pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -14,4 +14,5 @@ def read_file(filename=""):
     # Ouverture du fichier en mode lecture ('r') avec encodage UTF-8
     with open(filename, 'r', encoding='utf-8') as f:
         # Lire tout le contenu du fichier et l'afficher
-        print(f.read(), end="")  # end="" pour ne pas ajouter de nouvelle ligne supplémentaire
+        print(f.read(), end="")  # end="" pour ne pas ajouter
+        # de nouvelle ligne supplémentaire
