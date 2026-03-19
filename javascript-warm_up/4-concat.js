888ae7cf0178dfeff8234565426ce7505811530e
maxim880000 <mdholberton@gmail.com>
2026-03-19 15:23:24 +0100

    committed task 4 js

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// récupérer les deux premiers arguments passés
+const arg1 = process.argv[2]; 
+const arg2 = process.argv[3]; 
+
+console.log(arg1 + ' is ' + arg2); // affiche "arg1 is arg2"
