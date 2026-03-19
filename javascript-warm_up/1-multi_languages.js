2418607ac3952346d8835d41c49b0b5cdc1ae742
maxim880000 <mdholberton@gmail.com>
2026-03-19 15:01:09 +0100

    committed task 1 js

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const line1 = 'C is fun'; // première ligne
+const line2 = 'Python is cool'; // deuxième ligne
+const line3 = 'JavaScript is amazing'; // troisième ligne
+
+console.log(line1); // affiche la première ligne
+console.log(line2); // affiche la deuxième ligne
+console.log(line3); // affiche la troisième ligne
