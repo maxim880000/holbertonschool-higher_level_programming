7fd272449803e96bc7a89adf49d86a152000a0fd
maxim880000 <mdholberton@gmail.com>
2026-03-27 10:45:45 +0100

    push all the locals files of js DOM

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      document.getElementById('hello').textContent = data.hello;
+    });
+});
