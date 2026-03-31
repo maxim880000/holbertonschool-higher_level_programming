90c2aa59a6317335e68e90f5bd73d585209b354d
maxim880000 <mdholberton@gmail.com>
2026-03-31 14:26:28 +0200

    committed all the answer of QCM python everything object

---
+++ python-everything_is_object/19-copy_list.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def copy_list(a_list):
+    # Retourne une nouvelle liste qui contient les mêmes éléments que a_list
+    return a_list[:]  # Le slicing [:] crée une copie indépendante de la liste
